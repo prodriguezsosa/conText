5a59d1677b7d14c023f09b87911b7e5d2ad26dcd
sofiaaj <47996697+sofiaaj@users.noreply.github.com>
2025-08-22 13:21:07 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -3,3 +3,15 @@
   packageStartupMessage(pkgname, " v", pd$Version, " successfully loaded. Note this version of the package comes with significant changes to the conText() function.
                         See Quick Start Guide for help getting started and instructions for accessing older versions of the package:\n https://github.com/prodriguezsosa/conText/blob/master/vignettes/quickstart.md");
 }
+
+.onLoad <- function(libname, pkgname) {
+  #if (identical(Sys.getenv("_R_CHECK_LIMIT_CORES_"), "TRUE")) {
+    Sys.setenv(
+      OMP_NUM_THREADS = "1",
+      OPENBLAS_NUM_THREADS = "1",
+      MKL_NUM_THREADS = "1",
+      VECLIB_MAXIMUM_THREADS = "1",
+      RCPP_PARALLEL_NUM_THREADS = "1"
+    )
+  #}
+}
