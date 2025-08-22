f2cec5d06c9784c867c640427c0628b09023a368
sofiaaj <47996697+sofiaaj@users.noreply.github.com>
2025-08-22 13:52:29 -0400

    Tests

--- R/zzz.R
+++ R/zzz.R
@@ -5,14 +5,14 @@
 }
 
 .onLoad <- function(libname, pkgname) {
-  #if (identical(Sys.getenv("_R_CHECK_LIMIT_CORES_"), "TRUE")) {
+  if (identical(Sys.getenv("_R_CHECK_LIMIT_CORES_"), "TRUE")) {
     Sys.setenv(
-      "OMP_THREAD_LIMIT" = 2,
+      OMP_THREAD_LIMIT = "2",
       OMP_NUM_THREADS = "1",
       OPENBLAS_NUM_THREADS = "1",
       MKL_NUM_THREADS = "1",
       VECLIB_MAXIMUM_THREADS = "1",
       RCPP_PARALLEL_NUM_THREADS = "1"
     )
-  #}
+  }
 }
